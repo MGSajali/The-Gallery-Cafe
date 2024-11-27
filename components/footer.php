49c03cfe8d563a26267dea9256066c2c53f25c6b
Sajali Supunya <sajalisupunya@gmail.com>
2024-11-27 11:19:18 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>email</h3>
+         <a href="mailto:gallerycafe@gmail.com">gallerycafe@gmail.com</a>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>07:00am to 11:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>location</h3>
+         <a href="#">2 Alfred House Road, Colombo, Western Province</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>contact number</h3>
+         <a href="tel:+94 112 582 162">+94 112 582 162</a>
+         
+      </div>
+
+
+   </section>
+
+   <div class="credit"> <a href="Contact.php"><span>Contact </span> </a><br><br>
+   <a href="About.php"><span>About</span> </a><br> <br>
+   <a href="home.php"><span>Privacy Policy</span> </a> <br> <br>
+   <a href="home.php"><span>Terms and Conditions</span></a> <br>
+    <br><span>&copy;</span>  <span><?= date('Y'); ?> </span><span>The Gallery Café.</span></div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
